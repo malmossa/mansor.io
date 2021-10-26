bb65c11c1b412ec16b5eeb64d355854493ab78f3
Mansor Almossa <malmossa@gmail.com>
2021-10-25 19:24:55 -0700

    Redo the site

--- js/plugins.js
+++ js/plugins.js
@@ -1,137 +1,33 @@
 /*========== NAVBAR TRANSPARENT TO SOLID ==========*/
-$(document).ready(function () { //when document(DOM) loads completely
-    checkScroll(); //check if page is scrolled
-    $(window).scroll(checkScroll); //get scroll position of window
-});
-
-function checkScroll() { //check if page is scrolled
-if ($(window).scrollTop() >= 300) { //if window is scrolled 300px or more
-    $('.navbar').addClass('solid'); //add class 'solid' to element with class 'navbar'
-} else { //if page is not scrolled 300px from top
-    $('.navbar').removeClass('solid'); //remove class 'solid' from navbar element
-}
-}
 
 
 /*========== ADD SOLID CLASS TO NAVBAR WHEN TOGGLED ==========*/
-$(document).ready(function () {
-	checkScroll();
-	$(window).scroll(checkScroll);
-	$('.navbar-toggler').click(function () {
-		if (!$('.navbar').hasClass('solid-toggle')) {
-			if ($(window).scrollTop() <= 300) {
-				$('.navbar').toggleClass('solid-toggle');
-			}
-		}
-	});
-});
 
 
-/*========== CLOSE MOBILE MENU ON CLICK & SMOOTH SCROLL TO LINK a[href^="#"] ==========*/
-$(document).on('click', 'a[href^="#"]', function (event) {
-    event.preventDefault();
-    $('.navbar-toggler').addClass('collapsed');
-    $('#navbarResponsive').removeClass('show');
+/*========== CLOSE MOBILE MENU ON CLICK & SMOOTH SCROLL TO LINK ==========*/
 
-    setTimeout(function () {
-        $('nav.navbar').removeClass('solid-toggle');
-    }, 300);
-
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 1000);
-});
 
 /*========== BOUNCING DOWN ARROW ==========*/
-$(document).ready(function () {
-    $(window).scroll(function () {
-        $('.arrow').css('opacity', 1 - $(window).scrollTop() / 250);
-    });
-});
 
 
 /*========== LIGHTBOX IMAGE GALLERY ==========*/
-$(document).ready(function () {
-    lightbox.option({
-        'resizeDuration': 600,
-        'wrapAround': true,
-        'imageFadeDuration': 500
-    });
-});
 
 
 /*========== MEET THE TEAM CAROUSEL ==========*/
-$(document).ready(function(){ //when document(DOM) loads completely
-    $('#team-carousel').owlCarousel({ //owlCarousel settings
-        autoplay: true, //set to false to turn off autoplay and only use nav
-        autoplayHoverPause: true, //set to false to prevent pausing on hover
-        loop: true, //set to false to stop carousel after all slides shown
-        autoplayTimeout: 8000, //time between transitions
-        smartSpeed: 1200, //transition speed
-        dotsSpeed: 1000, //transition speed when using dots/buttons
-        responsive : { //set number of items shown per screen width
-            0 : {
-                items: 1 //0px width and up display 1 item
-            },
-            768 : {
-                items: 2 //768px width and up display 2 items
-            },
-            992 : {
-                items: 3 //992px width and up display 3 items
-            }
-        }
-    });
-  });
 
 
 /*========== SKILLS COUNTER ==========*/
-$(document).ready(function () {
-    $('.counter').counterUp({
-        delay: 10,
-        time: 3000,
-        beginAt: 0
-    });
-});
 
 
 /*========== CLIENTS CAROUSEL ==========*/
-$(document).ready(function(){ //when document(DOM) loads completely
-    $('#clients-carousel').owlCarousel({ //owlCarousel settings
-        autoplay: true, //set to false to turn off autoplay and only use nav
-        autoplayHoverPause: true, //set to false to prevent pausing on hover
-        loop: true, //set to false to stop carousel after all slides shown
-        autoplayTimeout: 8000, //time between transitions
-        smartSpeed: 1600, //transition speed
-        dotsSpeed: 1000, //transition speed when using dots/buttons
-        responsive : { //set number of items shown per screen width
-            0 : {
-                items: 1 //0px width and up display 1 item
-            },
-            768 : {
-                items: 2 //768px width and up display 2 items
-            }
-        }
-    });
-  });
+
 
 /*========== TOP SCROLL BUTTON ==========*/
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 500) {
-            $('.top-scroll').fadeIn();
-        } else {
-            $('.top-scroll').fadeOut();
-        }
-    });
-});
 
 
-/*========== MAKE ALL ANIMATION "FADEINUP" ON MOBILE ==========*/
-$(document).ready(function () {
-    if ($(window).width() < 768) {
-        $('div').attr('data-animation', 'animate__animated animate__fadeInUp');
-    }
-});
+/*========== MAKE ALL ANIMATION "FADEINUP" ON MOBILE 
+
+**Remember to use "animate__animated animate__fadeInUp" ==========*/
 
 
 
@@ -197,7 +93,7 @@ $(function () {
                   var messageText = data.message;
 
                   // let's compose Bootstrap alert box HTML
-                  var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable fade show" role="alert"><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>' + messageText + '</div>';
+                  var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
 
                   // If we have messageAlert and messageText
                   if (messageAlert && messageText) {
